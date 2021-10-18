40a123541de91e1b0a360c962d6f085dfe2f54e0
leobiriba <leonardoxcontato@gmail.com>
2021-10-17 21:23:36 -0300

    Estudando codigo javascript com o firebase, banco de dados nao relacional. O codigo permite fazer login e cadastro

---
+++ firebaseAuth/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6a153ac265ff637da5fef369afe6e747d0778805',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6a153ac265ff637da5fef369afe6e747d0778805',
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v199',
+            'version' => '199.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'reference' => '1a4e5d3ffe128f8eb8a17e50616462d3c804e11f',
+            'dev_requirement' => true,
+        ),
+    ),
+);
--- firebaseAuth/web/index.php
+++ firebaseAuth/web/index.php
@@ -1,1 +1,3 @@
-<?php include("bdIII.html");?>
+<?php 
+include('bdIII.html');
+?>
